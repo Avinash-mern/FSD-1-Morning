3b3b9e6d0996c0f583360fc111669ddd986f8d23
Avinash-mern <codeprepacademy123@gmail.com>
2024-12-06 08:14:34 +0530

    Create Timecomplexity.java

---
+++ Timecomplexity.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        //Time coplexity;
+        // int a = 4;
+        // int b = 2;
+        // System.out.print(a+b); //mathematical expression 
+        // // T.c--> O(1)
+        //O(1) approach
+        // int n = 10;
+        // int sum = (n*(n+1))/2; //math ka expression
+        // System.out.print(sum); 
+        
+        //using loop
+        //O(N)
+        // int n = 10;
+        // int sum = 0;
+        // for(int i = 1; i<=n; i++){
+        //     sum = sum + i;
+        // }
+        // System.out.print(sum);
+        
+        //O(N) -->
+        // for(int i = 0; i<n; i++){
+            
+        // // }
+        // for(int i = 0; i<n; i++){
+            
+        // // }
+        // Total T.c --> O(N) + O(N) --> O(2N)-->O(N);
+        
+        // for(int i = 1; i<=n; i++){
+        //     for(int j = 1; j<=n; j++){
+                
+        //     }
+        // }
+        // -->T.c--> O(n*n)-->O(N^2)
+        
+        // for(int i = 1; i<n; i++){
+        //     for(int j = 1; j<m; j++){
+        //         for(int k = 1; k<o; k++){
+                    
+        //         }
+        //     }
+        // }
+        // T.C --> O(n*m*o) -->O(n^3)
+        
+        
+    }
+}
